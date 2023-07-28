006a3e4acabb011f93f2428d8a649b7aa2b1b4a3
Harsha-Jagadish <harshajagadish99@gmail.com>
2023-07-28 12:03:18 -0700

    Added the router tag for link to work in Hero comp

--- portfolio/src/app/components/Hero.tsx
+++ portfolio/src/app/components/Hero.tsx
@@ -4,6 +4,8 @@ import React from 'react'
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
 import Image from "./Image";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 type Props = {}
 
 export default function Hero({}: Props) {
@@ -13,6 +15,7 @@ export default function Hero({}: Props) {
         delaySpeed: 2000,
     })
   return (
+    <Router>
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
         <BackgroundCircles />
         <img
@@ -20,13 +23,32 @@ export default function Hero({}: Props) {
         src="/1627366534091.jpeg" 
         />
 
-        <div> 
+        <div className="z-20"> 
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]"> Software Engineer</h2>
         <h1 className="text-5xl lg:text-6xl font-semibold px-10">
             <span className="mr-3">{text}</span>
             <Cursor cursorColor="orange"/>
         </h1>
+        <div className="pt-5">
+            <Link href="#about">
+            <button className="heroButton">About</button>
+            </Link>
+
+            <Link href="#experience">
+            <button className="heroButton">Experience</button>
+            </Link>
+
+            <Link href="#skills">
+            <button className="heroButton">Skills</button>
+            </Link>
+
+            <Link href="projects">
+            <button className="heroButton">Projects</button> 
+            </Link>
+
+        </div>
         </div>
     </div>
+    </Router>
   )
 }
