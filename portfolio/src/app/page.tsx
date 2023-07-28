e995210462d16f91977dca3ec93910ae8d23c50c
Harsha-Jagadish <harshajagadish99@gmail.com>
2023-07-28 11:36:34 -0700

    Added more components to the main page

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -2,10 +2,11 @@ import Image from 'next/image'
 import Head from 'next/head'
 import styles from "../styles/Home.modules.css";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
     <Head>
       <title>Harsha's Portfolio</title>
     </Head>
@@ -15,6 +16,9 @@ export default function Home() {
     <Header />
 
     {/* Hero */}
+    <section id="hero" className="snap-center">
+      <Hero />
+    </section>
 
     {/* About */}
 
