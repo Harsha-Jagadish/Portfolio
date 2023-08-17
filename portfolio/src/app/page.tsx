aea84cd8d9841cde22492aa839ddf7de006307ab
Harsha-Jagadish <harshajagadish99@gmail.com>
2023-08-16 21:26:56 -0700

    Project section layout complete

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -5,6 +5,8 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import WorkExperience from "./components/WorkExperience";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 export default function Home() {
   return (
@@ -36,7 +38,17 @@ export default function Home() {
 
     {/* Skills */}
 
+    <section id="skills" className="snap-start">
+      <Skills />
+
+    </section>
+
     {/* Projects */}
+
+    <section id="projects" className="snap-center">
+      <Projects />
+      
+    </section>
 
     {/* Contact Me */}
   </div>
